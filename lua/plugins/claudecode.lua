4cfd888b2dcc8339a1e1a1cf4d676f247184e44c
jin <pnescene@icloud.com>
2025-12-29 20:27:13 +0900

    refactor(claudecode): change keymap prefix to <leader>c and set terminal width to 40%

--- lua/plugins/claudecode.lua
+++ lua/plugins/claudecode.lua
@@ -2,6 +2,9 @@ return {
   "coder/claudecode.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    terminal = {
+      split_width_percentage = 0.40, -- ターミナル幅を40%に設定
+    },
     diff_opts = {
       open_in_new_tab = true,
       hide_terminal_in_new_tab = false,
@@ -13,13 +16,13 @@ return {
     vim.keymap.set("t", "<C-s>", [[<C-\><C-n>]], { desc = "Scroll mode" })
   end,
   keys = {
-    { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-    { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
-    { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-    { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-    { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-    { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-    { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
-    { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+    { "<leader>cc", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+    { "<leader>cf", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+    { "<leader>cr", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+    { "<leader>cC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+    { "<leader>cb", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+    { "<leader>cs", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+    { "<leader>ca", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+    { "<leader>cd", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
   },
 }
